d1983293c4aa1498dc271672e7eb8e30e6c14815
Jacob Higgins <higginsjake909@gmail.com>
2021-04-14 17:19:08 -0500

    first

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+
+int main(void)
+{
+    Serial.begin(115200);
+
+    while(1)
+    {
+        printf("Test: \n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+
+    return 0;
+}
+
+
+
+
+void setup() 
+{
+    main();
+}
+
+void loop() 
+{
+    delay(1000);
+}
